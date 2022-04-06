a174146d8027adfc5f2e9f3778ef40a1d6b330e0
denis <permwed@gmail.com>
2022-04-06 09:23:45 +0500

    add routing

---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,11 @@
+// Libraries
+import React, { FC } from "react";
+import { Routes, Route } from "react-router-dom";
+
+const AppRouter: FC = () => {
+  const auth = true;
+
+  return <div>auth</div>;
+};
+
+export default AppRouter;
