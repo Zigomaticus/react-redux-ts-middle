ccaaf0f02cc8e07549bb94e515a80f8ed006e1d1
denis <permwed@gmail.com>
2022-06-14 20:04:49 +0500

    work work

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,32 @@
-import { Row } from "antd";
+import { Menu, Row } from "antd";
 import { Header } from "antd/lib/layout/layout";
 import React, { FC } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar: FC = () => {
+  const navigate = useNavigate();
+  const auth = true;
+
   return (
     <Header>
-      <Row justify="end">sss</Row>
+      <Row justify="end">
+        {auth ? (
+          <>
+            <div style={{ color: "white" }}>Denis</div>
+            <Menu theme="dark" mode="horizontal" selectable={false}>
+              <Menu.Item onClick={() => console.log("Exit")} key={1}>
+                Выйти
+              </Menu.Item>
+            </Menu>
+          </>
+        ) : (
+          <Menu theme="dark" mode="horizontal" selectable={false}>
+            <Menu.Item onClick={() => navigate("/login")} key={1}>
+              Логин
+            </Menu.Item>
+          </Menu>
+        )}
+      </Row>
     </Header>
   );
 };
